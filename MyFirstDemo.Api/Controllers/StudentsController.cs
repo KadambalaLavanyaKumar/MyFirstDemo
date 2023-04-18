a098ca1cc238e801b0d46c59c545f82e9152cd87
KadambalaLavanyaKumar <lavanyakumar.cse@gmail.com>
2023-04-18 18:55:05 +0530

    Add project files.

---
+++ MyFirstDemo.Api/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstDemo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "john", "jane", "Mark", "emily", "David" };
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ MyFirstDemo.Api/Models/RegionDto/UpdateRegionRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirstDemo.Api.Models.RegionDto
+{
+    public class UpdateRegionRequestDto
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
