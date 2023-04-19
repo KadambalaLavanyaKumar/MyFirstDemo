78f1ec576ca844b39f802c2e70b9ea76044fac3c
KadambalaLavanyaKumar <lavanyakumar.cse@gmail.com>
2023-04-19 19:16:27 +0530

    Create AutoMapperProfiles.cs

---
+++ MyFirstDemo.Api/Mappings/AutoMapperProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MyFirstDemo.Api.Models.Domain;
+using MyFirstDemo.Api.Models.Dto;
+using MyFirstDemo.Api.Models.RegionDto;
+
+namespace MyFirstDemo.Api.Mappings
+{
+    public class AutoMapperProfiles : Profile
+    {
+       public AutoMapperProfiles() {
+            CreateMap<Region,RegionDto>().ReverseMap();
+            CreateMap<AddRegionRequestDto,Region>().ReverseMap();
+            CreateMap<UpdateRegionRequestDto, Region>().ReverseMap();
+            CreateMap<AddWalkRequestDto, Walk>().ReverseMap();
+            CreateMap<Walk, WalkDto>().ReverseMap();
+            CreateMap<Difficulty, DifficultyDto>().ReverseMap();
+            CreateMap<UpdateWalkRequestDto, Walk>().ReverseMap();
+        }
+    }
+
+  
+}
