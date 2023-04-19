04236c33921ac9799d438ab0dd6cf4ebb6502f26
KadambalaLavanyaKumar <lavanyakumar.cse@gmail.com>
2023-04-19 19:17:21 +0530

    Create IWalkRepository.cs

---
+++ MyFirstDemo.Api/Repositories/IWalkRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyFirstDemo.Api.Models.Domain;
+
+namespace MyFirstDemo.Api.Repositories
+{
+    public interface IWalkRepository
+    {
+        Task<Walk> CreatAsync(Walk walk);
+        Task<List<Walk>> GetAllAsync();
+        Task<Walk?> GetByIdAsync(Guid id);
+        Task<Walk?> UpdateAsync(Guid id, Walk walk);
+        Task<Walk?> DeleteASync(Guid id);
+    }
+}
