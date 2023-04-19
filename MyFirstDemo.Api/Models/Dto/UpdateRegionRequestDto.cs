9c61611fe27e36090c9dfc96bc50fa060691eb9c
KadambalaLavanyaKumar <lavanyakumar.cse@gmail.com>
2023-04-19 19:16:52 +0530

    Create UpdateRegionRequestDto.cs

---
+++ MyFirstDemo.Api/Models/Dto/UpdateRegionRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstDemo.Api.Models.RegionDto
+{
+    public class UpdateRegionRequestDto
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "code has to be minimum of 3 character")]
+        [MaxLength(3, ErrorMessage = "code has to be maximum of 3 character")]
+        public string Code { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "max length cannot exceed 100 charater")]
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
