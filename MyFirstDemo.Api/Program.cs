6d3a33cb4f604f8bf6b43bb3480832dadfaa50e6
KadambalaLavanyaKumar <lavanyakumar.cse@gmail.com>
2023-04-19 19:17:15 +0530

    Update Program.cs

--- MyFirstDemo.Api/Program.cs
+++ MyFirstDemo.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MyFirstDemo.Api.Data;
+using MyFirstDemo.Api.Mappings;
 using MyFirstDemo.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<NZWalksDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("NzWalksConnectionString")));
 
 builder.Services.AddScoped<IRegionRepository,SQLRegionRepository>();
+builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
+
+builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
 var app = builder.Build();
 
